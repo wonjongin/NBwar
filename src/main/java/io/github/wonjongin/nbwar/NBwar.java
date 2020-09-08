5855e28f2504faa2f69420053fb7d75275089cea
wonjongin <59444360+wonjongin@users.noreply.github.com>
2020-09-08 18:19:34 +0900

    Add command system

--- src/main/java/io/github/wonjongin/nbwar/NBwar.java
+++ src/main/java/io/github/wonjongin/nbwar/NBwar.java
@@ -1,6 +1,10 @@
 package io.github.wonjongin.nbwar;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public final class NBwar extends JavaPlugin {
 
@@ -9,11 +13,33 @@ public final class NBwar extends JavaPlugin {
         // Plugin startup logic
         getLogger().info("Plugin enabled");
         getLogger().info("Thanks for using our plugin!!");
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Plugin disabled");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("n")) {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.RED + "Type the command to execute.");
+            } else if (args[0].equalsIgnoreCase("info")) {
+                sender.sendMessage(ChatColor.YELLOW + "A PVP plugin for minecraft 1.12.2");
+            } else if (args[0].equalsIgnoreCase("hello")) {
+                sender.sendMessage(ChatColor.GREEN + "Hello World!");
+            } else if (args[0].equalsIgnoreCase("java")) {
+                sender.sendMessage(ChatColor.GREEN + "Java is programming language!!");
+            } else if (args[0].equalsIgnoreCase("power")) {
+                sender.sendMessage(ChatColor.GREEN + "준비중");
+            } else {
+                sender.sendMessage(ChatColor.RED + "Command Not Found");
+            }
+            return true;
+        }
+        return false;
     }
 }
