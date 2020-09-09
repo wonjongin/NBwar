513298ba56f5d8c6d5507703194f011a4d8a7329
wonjongin <wji0227@naver.com>
2020-09-09 15:51:27 +0900

    Modify message

--- src/main/java/io/github/wonjongin/nbwar/NBwar.java
+++ src/main/java/io/github/wonjongin/nbwar/NBwar.java
@@ -44,7 +44,7 @@ public final class NBwar extends JavaPlugin {
             } else if (args[0].equalsIgnoreCase("heal")) {
                 sender.sendMessage(ChatColor.GREEN + "준비중...");
             } else {
-                sender.sendMessage(ChatColor.RED + "Command Not Found");
+                sender.sendMessage(ChatColor.RED + "Command Not Found!!");
             }
             return true;
         }
