0e0bba68d8c52dd597bb240ed10c7a7db005f669
wonjongin <wji0227@naver.com>
2020-09-09 19:09:34 +0900

    아이템 부여 코드, 서버 입장 메소드 추가

--- src/main/java/io/github/wonjongin/nbwar/NBwar.java
+++ src/main/java/io/github/wonjongin/nbwar/NBwar.java
@@ -1,16 +1,21 @@
 package io.github.wonjongin.nbwar;
 
 import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public final class NBwar extends JavaPlugin {
+public final class NBwar extends JavaPlugin implements Listener {
     Runtime runtime = Runtime.getRuntime();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -28,6 +33,8 @@ public final class NBwar extends JavaPlugin {
     @EventHandler
     public void join(PlayerJoinEvent event){
         event.setJoinMessage(ChatColor.GOLD+"입장하신 것을 환영합니다!!");
+        Player player = event.getPlayer();
+        player.sendMessage(ChatColor.YELLOW + "입장하신 것을 환영합니다!!");
     }
 
     @Override
@@ -63,5 +70,13 @@ public final class NBwar extends JavaPlugin {
             return true;
         }
         return false;
+    }
+    @EventHandler
+    public void breakBlock(BlockBreakEvent event){
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        ItemStack itemStack = new ItemStack(block.getType());
+        player.getInventory().addItem(itemStack);
+        player.sendMessage(ChatColor.GREEN + "Get block twice!!");
     }
 }
